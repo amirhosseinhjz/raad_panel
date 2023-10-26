0a61b40fe8684c0564ab1c1419bf3e2b870af9f4
amirhosseinhjz <amirh.hejazian@digikala.com>
2023-10-26 18:40:23 +0330

    company sms in signal

--- raad/signals.py
+++ raad/signals.py
@@ -3,18 +3,21 @@ from django.dispatch import receiver
 from raad.models import Company, Device, MessengerAdmin, SyncServerUrl, ErrorLog, SyncDataAPI
 from raad.UseCases.data_provider import get_company_full_data
 import json
+from raad.UseCases import sms_service
 
 
 @receiver(post_save, sender=Company)
 def company_post_save(sender, instance, created, **kwargs):
     if created:
-        return
+        sms_service.send_succesful_buy(instance.user.username)
     data = get_company_full_data(instance)
     sync_company(data)
 
 
 @receiver(post_save, sender=Device)
 def device_post_save(sender, instance, created, **kwargs):
+    # if created:
+    #     sms_service.send_succesful_buy(instance.user.username)
     company = instance.company
     data = get_company_full_data(company)
     sync_company(data)
