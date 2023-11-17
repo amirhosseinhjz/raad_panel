1ac995cfab8e6be2e0d57a22af330cc7980f1032
amirhosseinhjz <amirh.hejazian@digikala.com>
2023-11-17 23:53:00 +0330

    fix ip limitation

--- raad/utils.py
+++ raad/utils.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseForbidden
 from raad.models import AllowedIp
 import random
 
+
 def whitelist_ip(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
@@ -11,7 +12,7 @@ def whitelist_ip(view_func):
         if AllowedIp.objects.filter(ip=user_ip).exists():
             return view_func(request, *args, **kwargs)
         else:
-            return HttpResponseForbidden("Access denied.")
+            return HttpResponseForbidden("Access denied for ip: " + user_ip)
 
     return _wrapped_view
 
