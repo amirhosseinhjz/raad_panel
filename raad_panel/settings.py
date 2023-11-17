d2e8c08343bbe21790dac436a9c5fa5b142492df
amirhosseinhjz <amirh.hejazian@digikala.com>
2023-11-18 00:02:55 +0330

    add cloudflare header for user ip

--- raad_panel/settings.py
+++ raad_panel/settings.py
@@ -127,6 +127,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
