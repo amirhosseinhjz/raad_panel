f2c3b95f34a00166c2cd7a54ecf0a857d8983aa9
amirhosseinhjz <amirh.hejazian@digikala.com>
2023-11-18 11:28:47 +0330

    add cloudflare header for user ip

--- raad/utils.py
+++ raad/utils.py
@@ -7,12 +7,13 @@ import random
 def whitelist_ip(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        user_ip = request.META.get('HTTP_X_FORWARDED_FOR', '') or request.META.get('REMOTE_ADDR')
+        user_ip = request.META.get('HTTP_X_FORWARDED_FOR', '')
+        user_ip2 = request.META.get('REMOTE_ADDR')
 
         if AllowedIp.objects.filter(ip=user_ip).exists():
             return view_func(request, *args, **kwargs)
         else:
-            return HttpResponseForbidden("Access denied for ip: " + user_ip)
+            return HttpResponseForbidden("Access denied for ip: " + user_ip + ', ' + user_ip2)
 
     return _wrapped_view
 
